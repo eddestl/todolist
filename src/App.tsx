410853a1e8b107b107d5666ece943e78bd10b0a1
amanda <amanda@swedsec.se>
2026-03-17 20:06:33 +0100

    Toggle button, delete button with functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import { useState } from 'react'
 import './assets/app.scss'
 import Container from "react-bootstrap/Container";
-import { ListGroup, ListGroupItem, Placeholder } from 'react-bootstrap';
+import { ListGroup } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
-import TodoItemCounter from './components/TodoItemCounter'
 import { Todo } from './types/Todo.types';
 
   const initialTodos: Todo[] = [
-        { id: 1, title: 'Curl hair', completed: true },
+    { id: 1, title: 'Curl hair', completed: true },
     { id: 2, title: 'Paint nails', completed: true },
     { id: 3, title: 'Face mask', completed: false },
   ]
@@ -30,8 +29,18 @@ function App() {
     setInputTodoTitle("");
   }
 
+  const handleToggleTodo = (item:Todo) => {
+    item.completed = !item.completed;
 
- 
+    console.log(item.completed)
+    setTodos([...todos]);
+  }
+
+  const handleDeleteTodo = (item:Todo) => {
+    setTodos(todos.filter(t => t.id !== item.id));
+  }
+
+
 
   return (
     <Container>
@@ -50,13 +59,31 @@ function App() {
         </InputGroup>
 
       </Form>
+      {todos.length ? (
+        <>
       <ListGroup className="todolist mb-3">
         {todos.map(item =>(
-          <ListGroup.Item className= {item.completed ?"completed" :""} key={item.id}>
+          <ListGroup.Item className = {item.completed ? "completed" :""} key={item.id}>
             <span className="todo-title">{item.title}</span>
+            <div>            
+              <Button 
+              onClick={() => handleToggleTodo(item)}
+            size="sm"
+            variant="outline-warning">Toggle status</Button>
+            <Button 
+              onClick={() => handleDeleteTodo(item)}
+            size="sm"
+            variant="outline-danger">Delete item</Button>
+            </div>
+
           </ListGroup.Item>
         ))}
       </ListGroup>
+      <p className="text-muted"> {todos.filter(item => item.completed).length} / {todos.length} completed</p>
+      </>
+
+      ) :(<p >You are done for the day!</p>)
+      } 
 
     </Container>
   )
